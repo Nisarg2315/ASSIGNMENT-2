4cbdc2ca63b52510edc523dd813740fdbdb5425f
Nisarg2315 <nisarg2315@gmail.com>
2025-08-31 22:16:24 +0530

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+int a;
+
+printf("Enter the value of a :");
+scanf("%d", &a);
+
+if(a%7 == 0){
+printf("%d is divisible by 7.\n");
+}
+else{
+printf("d is not divisible by 7.\n");
+}
+
+return 0;
+}
