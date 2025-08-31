27a6bbdc8ac52b701f77ae6293bb1fd687371662
Nisarg2315 <nisarg2315@gmail.com>
2025-08-31 22:13:14 +0530

    Add files via upload

---
+++ 01.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+int a,b;
+printf("Enter number\n");
+scanf("%d %d", &a,&b);
+if(a>b)
+{
+printf("a is larger value");
+}
+else
+{
+printf("b is larger value");
+}
+return 0;
+}
